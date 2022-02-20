388f0cc6745303b8b13b522570042f08d0f69259
simalina554 <98891098+simalina554@users.noreply.github.com>
2022-02-20 13:20:58 +0300

    change

--- Miniwebsite/urls.py
+++ Miniwebsite/urls.py
@@ -22,9 +22,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('firsrapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [path ('__debug__/', include('debug_toolbar.urls'))] + urlpatterns
     urlpatterns+= (static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) +
                   static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 
